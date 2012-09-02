1366ad8d8155b9a392862248a760fe265d5b5de2
lusob <luis@sobrecueva.com>
2012-09-02 10:13:50 -0500

    several improvements

--- urls.py
+++ urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 #
 urlpatterns = patterns('',
-    url(r'^$', 'f1mapp.views.home', name='home'),
-    url(r'^info$', 'f1mapp.views.info', name='info'),
-)
+                       url(r'^$', 'f1mapp.views.home', name='home'),
+                       url(r'^info$', 'f1mapp.views.info', name='info'),
+                       url(r'^error$', 'f1mapp.views.error', name='error'),
+                       url(r'^test$', 'f1mapp.views.test', name='test'),
+                       url(r'^data$', 'f1mapp.views.data', name='data'),
+                       url(r'^mapeditor$', 'f1mapp.views.mapeditor', name='mapeditor'),
+                       )
--- views.py
+++ views.py
@@ -11,3 +11,13 @@ def home(request):
     return render_to_response('f1mapp.html');
 def info(request):
     return render_to_response('info.html');
+def error(request):
+    return render_to_response('error.html');
+def test(request):
+    return render_to_response('test.html');
+def mapeditor(request):
+    return render_to_response('f1map_editor.html');
+def data(request):
+    import urllib2
+    data = urllib2.urlopen('http://qa.lusob.com/cars_data.json').read()
+    return render_to_response('data.html', {"data":data});
